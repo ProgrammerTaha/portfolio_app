962becb78212520c7b83d211931ee15382a664f0
Taha Saad <programmer.taha.1y@gmail.com>
2021-03-28 12:49:41 +0300

    adding download button

--- lib/middle.dart
+++ lib/middle.dart
@@ -26,22 +26,30 @@ class MiddleScreen extends StatelessWidget {
                 ProjectWidget(
                     title: 'Recipes app',
                     func: () {
-                      launch('');
+                      launch(
+                        'https://drive.google.com/file/d/1eHvpLLpjp6aeNgFiHthtP8jVV9WDBTUM/view?usp=sharing',
+                      );
                     }),
                 ProjectWidget(
                     title: 'Quizes app',
                     func: () {
-                      launch('');
+                      launch(
+                        'https://drive.google.com/file/d/1ZDmdYHt4x8N5a4x7DW7R-Wo_wn54O8-C/view?usp=sharing',
+                      );
                     }),
                 ProjectWidget(
-                    title: 'Personal expanses tracker app',
+                    title: 'expanses tracker',
                     func: () {
-                      launch('');
+                      launch(
+                        'https://drive.google.com/file/d/1VK0PjWKlP0crSxtYInNteF5oQfson6iY/view?usp=sharing',
+                      );
                     }),
                 ProjectWidget(
                     title: 'Portfolio App',
                     func: () {
-                      launch('');
+                      launch(
+                        'https://drive.google.com/file/d/10lijCHv-3Wnjk3ix4rOxDbp8YjGnigSD/view?usp=sharing',
+                      );
                     }),
 
                 // ProjectWidget(
@@ -80,21 +88,35 @@ class ProjectWidget extends StatelessWidget {
   const ProjectWidget({required this.title, required this.func});
   @override
   Widget build(BuildContext context) {
-    return TextButton(
-      style: ButtonStyle(
-        shape: MaterialStateProperty.all(Vx.roundedSm as OutlinedBorder?),
-        overlayColor: MaterialStateProperty.all(Coolors.secondaryColor),
-      ),
-      onPressed: func,
-      child: title.text.bold.white.xl.wide.center
-          .make()
-          .box
-          .p8
-          .roundedLg
-          .alignCenter
-          // .square(200)
-          .neumorphic(color: Vx.green700, elevation: 5.0, curve: VxCurve.flat)
-          .make(),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        TextButton(
+          style: ButtonStyle(
+            shape: MaterialStateProperty.all(Vx.roundedSm as OutlinedBorder?),
+            overlayColor: MaterialStateProperty.all(Coolors.secondaryColor),
+          ),
+          onPressed: func,
+          child: title.text.bold.white.xl.wide.center
+              .make()
+              .box
+              .p8
+              .roundedLg
+              .alignCenter
+              // .square(200)
+              .neumorphic(
+                  color: Vx.green700, elevation: 5.0, curve: VxCurve.flat)
+              .make(),
+        ),
+        ElevatedButton(
+          onPressed: func,
+          child: Text('Download'),
+          style: ButtonStyle(
+            shape: MaterialStateProperty.all(Vx.roundedSm as OutlinedBorder?),
+            overlayColor: MaterialStateProperty.all(Coolors.secondaryColor),
+          ),
+        ),
+      ],
     );
   }
 }
